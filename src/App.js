9d7166434ab86d298690855d6b6789916f47f5d0
Samagent97 <sammccarthy090@gmail.com>
2022-09-11 13:39:37 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import { Component } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Component, useState } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Cart from "./pages/cart_Page/cart";
 import Category from "./pages/category_Page/category";
 import Product from "./pages/product_Page/product";
 import { path } from "./routes";
+import Card from "./components/Card/Card";
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BrowserRouter>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/product'>
+            
+          </Link>
+          <Link to='/cart'></Link>
+        </div>
           <Routes>
             <Route path={path.category} exact element={<Category />} />
             <Route path={path.product} exact element={<Product />} />
